d62d1fa05ea2e1a8c6eae51375ebbecfc8a11ca0
김시언 <rlatldjs123@gmail.com>
2022-03-31 08:44:35 +0000

    Upload New File

---
+++ 실습03_막대색칠하기/Main.java
@@ -0,0 +1,21 @@
+package 막대색칠하기;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int result[] = new int[N+1];
+		result[1] = 2;
+		result[2] = 5;
+		
+		for(int i=3; i<=N; i++) {
+			result[i] = result[i-1] * 2 + result[i-2];
+		}
+		
+		System.out.println(result[N]);
+		
+		sc.close();
+	}
+}
