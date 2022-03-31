57ae40ab9c38d0b1ce7394c33090379f618b4b8d
권덕주 <djg4053@gmail.com>
2022-03-31 04:38:58 +0000

    Update 실습03_막대색칠하기/실습03_막대색칠하기_권덕주.java

---
+++ 실습03_막대색칠하기/실습03_막대색칠하기_권덕주.java
@@ -0,0 +1,22 @@
+
+public class 실습03_막대색칠하기 {
+
+	static int N = 6;
+	
+	static int dp[];
+	
+	public static void main(String[] args) {
+		
+		dp = new int[N + 5];
+		dp[0] = 1;
+		dp[1] = 2;
+		
+		for (int i = 2; i <= N; i++) {
+			dp[i] = dp[i - 1] * 2 + dp[i - 2];
+		}
+		
+		System.out.print("f(" + N + ") = ");
+		System.out.println(dp[N]);  // dp[6] = 169		
+	}
+
+}
