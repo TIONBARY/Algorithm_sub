776a5849f541f247052af7188cbf93ed553ed70f
권덕주 <djg4053@gmail.com>
2022-04-05 13:42:19 +0000

    Update 과제04_회전초밥/과제04_회전초밥_권덕주.java

--- 과제04_회전초밥/과제04_회전초밥_권덕주.java
+++ 과제04_회전초밥/과제04_회전초밥_권덕주.java
@@ -8,6 +8,11 @@ public class 과제04_회전초밥 {
 	static int N, d, k, c;
 	static int arr[];
 	
+	// 원형으로 된 초밥 테이블을 모사하기 위해서 배열을 N에서 N + k - 1개로 크기를 늘린다음 최초 k - 1개를 복사한다.
+	// 그 후 k개 만큼의 접시를 집은 상태에서 가지수를 세준다. 이를 시작 접시가 n - 1일때까지 반복한다.
+	// Map은 문제를 다시 읽어보니까 초밥 번호 숫자의 제한이 있어서 필요가 없다. index로 접근 가능하기때문에 배열로 충분하다.
+
+
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String line[] = br.readLine().split(" ");
