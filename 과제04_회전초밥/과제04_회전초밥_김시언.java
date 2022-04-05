5afd3babc88706358d603e8305b52c9294b68b9a
김시언 <rlatldjs123@gmail.com>
2022-04-05 07:53:59 +0000

    Update Main2.java

--- 과제04_회전초밥/과제04_회전초밥_김시언.java
+++ 과제04_회전초밥/과제04_회전초밥_김시언.java
@@ -1,10 +1,8 @@
-package Jungol2577;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Main2 {
+public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String s[];
